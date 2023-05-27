9c12627c1f5af5d97756bf7c70554ed9b55fd9d4
Felix J <felix.hannes.fj@gmail.com>
2023-05-27 14:14:34 +0200

    gridview

--- lib/layouts.dart
+++ lib/layouts.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
 
 Widget mainWidget() {
-  return Row(
-    children: [
-      container(),
-      container()
-    ],
+  // return Row(
+  //   children: [
+  //     container(),
+  //     container()
+  //   ],
+  // );
+  return gridView();
+}
+
+Widget gridView() {
+  return GridView.extent(
+    maxCrossAxisExtent: 150,
+    children: containerList(25),
+    crossAxisSpacing: 10,
+    mainAxisSpacing: 10,
+    padding: const EdgeInsets.all(5),
   );
+}
+
+containerList(int i) {
+  return List.generate(i, (index) => Container(
+    decoration: BoxDecoration(
+      color: const Color(0x85A02525),
+      border: Border.all(
+          color: Colors.amber,
+          width: 5
+      ),
+      borderRadius: BorderRadius.circular(10),
+    ),
+    child: Image.network("https://picsum.photos/100"),
+  ));
 }
 
 Widget container() {
@@ -21,7 +46,7 @@ Widget container() {
           borderRadius: BorderRadius.circular(10),
         ),
         margin: const EdgeInsets.all(5),
-        child: Image.network("https://picsum.photos/100"),
+        child: Image.network("https://picsum.photos/150"),
       )
   );
 
