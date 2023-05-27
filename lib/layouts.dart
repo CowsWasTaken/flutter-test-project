2944426cd4a0ccb33f0e479b3e69be6cb94e585a
Felix J <felix.hannes.fj@gmail.com>
2023-05-27 14:42:44 +0200

    stack

--- lib/layouts.dart
+++ lib/layouts.dart
@@ -20,34 +20,42 @@ Widget gridView() {
   );
 }
 
-containerList(int i) {
-  return List.generate(i, (index) => Container(
-    decoration: BoxDecoration(
-      color: const Color(0x85A02525),
-      border: Border.all(
-          color: Colors.amber,
-          width: 5
-      ),
-      borderRadius: BorderRadius.circular(10),
-    ),
-    child: Image.network("https://picsum.photos/100"),
-  ));
+List<Widget> containerList(int i) {
+  return List.generate(
+      i,
+      (index) => Stack(
+            alignment: const Alignment(0, 0.7),
+            children: [
+              const CircleAvatar(
+                radius: 100,
+                backgroundImage: NetworkImage("https://picsum.photos/100"),
+
+              ),
+              Container(
+                padding: const EdgeInsets.all(5),
+                decoration: const BoxDecoration(
+                  color: Colors.black45,
+                ),
+                child: const Text("Schön",
+                style: TextStyle(
+                  color: Colors.cyan,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20
+                )),
+              )
+            ],
+          ));
 }
 
 Widget container() {
   return Expanded(
       child: Container(
-        decoration: BoxDecoration(
-          color: const Color(0x85A02525),
-          border: Border.all(
-            color: Colors.amber,
-            width: 5
-          ),
-          borderRadius: BorderRadius.circular(10),
-        ),
-        margin: const EdgeInsets.all(5),
-        child: Image.network("https://picsum.photos/150"),
-      )
-  );
-
+    decoration: BoxDecoration(
+      color: const Color(0x85A02525),
+      border: Border.all(color: Colors.amber, width: 5),
+      borderRadius: BorderRadius.circular(10),
+    ),
+    margin: const EdgeInsets.all(5),
+    child: Image.network("https://picsum.photos/150"),
+  ));
 }
