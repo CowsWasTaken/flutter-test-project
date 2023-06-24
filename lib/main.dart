b8d4ea91a7c3c100d4d527e97c946741392dc28f
Felix J <felix.hannes.fj@gmail.com>
2023-06-24 13:37:09 +0200

    added popupmenu

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -8,14 +6,11 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(title: "Superhero App", home: DrawerExample());
     // MyExampleSnackbar());
   }
-
-
 }
 
 class DrawerExample extends StatefulWidget {
@@ -23,11 +18,9 @@ class DrawerExample extends StatefulWidget {
 
   @override
   State<StatefulWidget> createState() => _DrawerExampleState();
-
 }
 
-class _DrawerExampleState extends State<DrawerExample> with RestorationMixin{
-
+class _DrawerExampleState extends State<DrawerExample> with RestorationMixin {
   final RestorableInt _currentIndex = RestorableInt(0);
 
   @override
@@ -36,26 +29,46 @@ class _DrawerExampleState extends State<DrawerExample> with RestorationMixin{
   }
 
   createDrawerExample() {
+    var bottomNavBarItems = [
+      BottomNavigationBarItem(icon: Icon(Icons.account_box), label: "Account"),
+      BottomNavigationBarItem(icon: Icon(Icons.access_time), label: "Time"),
+    ];
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("My Drawer"),
-        ),
-        body: const Center(
-          child: Text("Draw me baby !!!"),
-        ),
-        drawer: Drawer(
-          child: buildDrawerElements(),
-        ),
-      bottomNavigationBar:
-      BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(
-              icon: Icon(Icons.account_box), label: "Account"
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.access_time), label: "Account"
-          ),
+      appBar: AppBar(
+        title: const Text("My Drawer"),
+        actions: [
+          PopupMenuButton(
+            onSelected: (value) => print(value),
+              itemBuilder: (context) => <PopupMenuEntry<String>>[
+                    const PopupMenuItem(
+                      value: "Teilen",
+                      child: ListTile(
+                        title: Text("Teilen"),
+                        leading: Icon(Icons.share),
+                      ),
+                    ),
+                    const PopupMenuDivider(),
+                    const PopupMenuItem(
+                      value: "Logout",
+                      child: ListTile(
+                        title: Text("Logout"),
+                        leading: Icon(Icons.logout),
+                      ),
+                    ),
+                  ])
         ],
+      ),
+      body: Center(
+        child: _BottomNavBarView(
+          key: UniqueKey(),
+          bottomNavigationBarItem: bottomNavBarItems[_currentIndex.value],
+        ),
+      ),
+      drawer: Drawer(
+        child: buildDrawerElements(),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: bottomNavBarItems,
         currentIndex: _currentIndex.value,
         onTap: (index) {
           setState(() {
@@ -109,7 +122,18 @@ class _DrawerExampleState extends State<DrawerExample> with RestorationMixin{
   }
 }
 
+class _BottomNavBarView extends StatelessWidget {
+  const _BottomNavBarView({Key? key, required this.bottomNavigationBarItem})
+      : super(key: key);
 
+  final BottomNavigationBarItem bottomNavigationBarItem;
+
+  @override
+  Widget build(BuildContext context) {
+    var displayText = bottomNavigationBarItem.label ?? 'No Content Provided';
+    return Text(displayText);
+  }
+}
 
 class MyButton extends StatelessWidget {
   int counter;
